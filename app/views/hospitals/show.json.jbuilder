ec9e3e11ab2a76e0787a92bfd7412217c89f03f7
Spencer Christopherson <schristopherson@gmail.com>
2016-05-08 15:26:44 -0600

    first commit

---
+++ app/views/hospitals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @hospital, :id, :name, :address, :description, :created_at, :updated_at
---
+++ db/migrate/20160508203600_create_hospitals.rb
@@ -0,0 +1,11 @@
+class CreateHospitals < ActiveRecord::Migration
+  def change
+    create_table :hospitals do |t|
+      t.string :name
+      t.text :address
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
